2c7522b723ca18c9bb18a7886b4c18159e086a8a
Potatoritos <32649997+Potatoritos@users.noreply.github.com>
2021-01-16 14:02:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,82 @@
 class Blob {
     constructor() {
-        this.x = 0;
-        this.y = 0;
-        
-        this.dx = 20;
-        this.dy = 5;
+        this.body = Matter.Bodies.rectangle(400, 0, 50, 50, {
+            mass: 100
+        });
+        this.onGround = true;
     }
 
-    move() {
-        this.x += this.dx;
-        this.y += this.dy;
+    jump() {
+        if (!this.onGround) return;
+        //Matter.Body.applyForce(this.body, this.body.position, {x:0, y:-2});
+        Matter.Body.setVelocity(this.body, {x:0, y:-8});
     }
 }
 
 class Game {
     start() {
-        this.canvas = document.getElementById("display");
-        this.ctx = this.canvas.getContext("2d");
-        this.blob = new Blob();
+        this.engine = Matter.Engine.create();
+        this.render = Matter.Render.create({
+            element: document.body,
+            engine: this.engine,
+            options: {
+                width: 800,
+                height: 600,
+                wireframes: false
+            }
+        });
 
+        this.blob = new Blob();
+        var ground = Matter.Bodies.rectangle(400, 500, 810, 60, {isStatic:true});
+        //ground.type = 'floor';
+
+        Matter.World.add(this.engine.world, [this.blob.body, ground]);
+
+        Matter.Engine.run(this.engine);
+        Matter.Render.run(this.render);
+
+        this.keys = {};
+
+        this.keyFuncs = {
+            38: { // up
+                up: () => {
+                    this.blob.jump();
+                },
+                down: () => {}
+            }
+        };
+        
         var t = this;
 
-        setInterval(function() {
-            t.loop();
-        }, 1000);
-    }
-
-    drawBlob() {
-        this.ctx.beginPath();
-        this.ctx.rect(this.blob.x, this.blob.y, 50, 50);
-        this.ctx.fillStyle = "#000000";
-        this.ctx.fill();
-        this.ctx.closePath();
-    }
-
-    loop() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        document.addEventListener('keydown', e => {
+            t.handleKeyDown(e);
+        }, false);
         
-        this.blob.move();
+        document.addEventListener('keyup', e => {
+            t.handleKeyUp(e);
+        }, false);
 
-        this.drawBlob();
-    }       
+        Matter.Events.on(this.engine, 'collisionStart', e => {
+            console.log(e.name);
+        });
+    }
+
+    handleKeyDown(e) {
+        if (this.keys[e.keyCode]) return;
+        this.keys[e.keyCode] = true;
+
+        if (e.keyCode in this.keyFuncs) {
+            this.keyFuncs[e.keyCode].up();
+        }
+
+        console.log(e.keyCode);
+    }
+
+    handleKeyUp(e) {
+        this.keys[e.keyCode] = false;
+
+        console.log(e.keyCode);
+    }
 }
 
 var game = new Game();
